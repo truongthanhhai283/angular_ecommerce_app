39557360574f85710f1c943779783b14d61db659
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2022-01-06 17:02:30 +0700

    Revert "Task/prepare content of main shared modules"

--- my-app/src/app/shared/files/files.module.ts
+++ my-app/src/app/shared/files/files.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FileSelectDirective,
-  FileDropDirective,
-  FileUploadModule,
-} from 'ng2-file-upload';
+
+
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FileUploadModule],
-  exports: [FileUploadModule, FileSelectDirective, FileDropDirective],
+  imports: [
+    CommonModule
+  ]
 })
-export class FilesModule {}
+export class FilesModule { }
