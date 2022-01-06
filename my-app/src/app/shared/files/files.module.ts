295d77d3c032b1b44d539b87384d05776c000295
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-06 17:00:38 +0700

    Task#4: Prepare the content of the main, shared modules

--- my-app/src/app/shared/files/files.module.ts
+++ my-app/src/app/shared/files/files.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {
+  FileSelectDirective,
+  FileDropDirective,
+  FileUploadModule,
+} from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, FileUploadModule],
+  exports: [FileUploadModule, FileSelectDirective, FileDropDirective],
 })
-export class FilesModule { }
+export class FilesModule {}
