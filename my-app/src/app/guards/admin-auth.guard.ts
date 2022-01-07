89c2357bb8875f8e985541f4b7156026418c93cc
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-07 21:37:34 +0700

    Task#15: Guards implement

--- my-app/src/app/guards/admin-auth.guard.ts
+++ my-app/src/app/guards/admin-auth.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
 
+import { AuthService } from '../services/auth/auth.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminAuthGuard implements CanActivate {
+  /**
+   *
+   */
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean {
+    if (
+      this.authService.isLoggedIn() &&
+      this.authService.currentUser &&
+      this.authService.currentUser.isAdmin
+    ) {
+      return true;
+    } else {
+      this.router.navigate(['/auth/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+      return false;
+    }
   }
-  
 }
