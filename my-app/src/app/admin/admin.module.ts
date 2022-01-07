bb8413d33732bd3415ec419e33fd0ee480ceebbe
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-07 21:14:17 +0700

    Task#14: Setting up application routes, and the usage of lazy loading

--- my-app/src/app/admin/admin.module.ts
+++ my-app/src/app/admin/admin.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ManageCategoriesComponent } from './components/manage-categories/manage-categories.component';
 import { ManageOrdersComponent } from './components/manage-orders/manage-orders.component';
 import { ManageUsersComponent } from './components/manage-users/manage-users.component';
-
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminRoutingModule } from './admin-routing.module';
 @NgModule({
   declarations: [
     ManageCategoriesComponent,
     ManageOrdersComponent,
     ManageUsersComponent,
+    DashboardComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
