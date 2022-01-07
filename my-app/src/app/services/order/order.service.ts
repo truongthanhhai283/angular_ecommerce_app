ff44e0513a4404979b915acc1fcd309a227adfc9
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-07 16:11:31 +0700

    Task#12: Order, invoice service implement

--- my-app/src/app/services/order/order.service.ts
+++ my-app/src/app/services/order/order.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/models/order';
+import { ErrorHandler } from 'src/app/shared/error-handler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  private orderUrl = `http://localhost:3000/orders`;
+  constructor(private http: HttpClient) {}
+  private errorHandler: ErrorHandler = new ErrorHandler();
 
-  constructor() { }
+  // For admin staff
+  getOrders(): Observable<Order[]> {
+    try {
+      return this.http.get<Order[]>(this.orderUrl);
+    } catch (error) {
+      this.errorHandler.handleError(error);
+    }
+  }
+
+  // For user staff
+  getUserOrders(orderId: number): Observable<Order> {
+    try {
+      return this.http.get<Order>(`${this.orderUrl}/${orderId}`);
+    } catch (error) {
+      this.errorHandler.handleError(error);
+    }
+  }
 }
