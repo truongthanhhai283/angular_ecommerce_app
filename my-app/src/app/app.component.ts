5fd2579b8fc27dae55758cb1fcc4a25840570538
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-08 21:47:02 +0700

    Task#17: Register component implement

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title = 'Ecommerce App';
+
+  /**
+   *
+   */
+  constructor(public authService: AuthService) {
+    authService.prepareUserData();
+  }
+  ngOnInit(): void {
+    this.authService.prepareUserData();
+  }
 }
