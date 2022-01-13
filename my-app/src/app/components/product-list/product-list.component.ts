5d526b81eb803f2251ae504edf717bdedaa440e5
HaiTruong283 <truongthanhhai283@gmail.com>
2022-01-13 09:14:35 +0700

    Task#21: Product list component implement

--- my-app/src/app/components/product-list/product-list.component.ts
+++ my-app/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  products: Product[];
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private productService: ProductService
+  ) {
+    if (route.snapshot.data.products) {
+      this.products = route.snapshot.data.products;
+    }
   }
 
+  ngOnInit(): void {}
+
+  productDetails(product: Product) {
+    this.router.navigate(['/products', product.id], {
+      queryParams: {
+        Name: product.name,
+      },
+    });
+  }
+
+  pushToCart(productId: number, quantity: number) {
+    if (this.authService.cartItem) {
+      this.productService.insertToCart(
+        productId,
+        this.authService.cartItem.id,
+        quantity
+      ).subscribe(res=>{
+        
+      });
+    }
+  }
 }
